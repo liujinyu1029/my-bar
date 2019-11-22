cab83361b74794a3a067ba01c869472fbe58bbcc
liujinyu <liujinyu@meicai.cn>
2019-11-22 18:38:52 +0800

    add indexDemo.vue

---
+++ vue/standard/indexDemo.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Author: 刘金宇
+ * @Date: 2019-11-20
+ * @Description: 
+ -->
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+  watch: {
+  },
+  // created 钩子
+  created() {
+  },
+  methods: {
+  },
+  // 依赖的组件
+  components:{
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
